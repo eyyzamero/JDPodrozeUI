e5c1a7a29ffb92f8b6035605bdca0564ca6ad93f
eyyzamero <eazymen789@gmail.com>
2023-04-04 20:38:39 +0200

    Application initialization

---
+++ src/configurations/configuration.production.ts
@@ -0,0 +1,4 @@
+export const configuration = {
+	version: '0.0.0',
+	production: true
+}
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,15 @@
+import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { configuration } from './configurations/configuration';
 
 import { AppModule } from './app/app.module';
 
+if (configuration.production) {
+	enableProdMode();
+
+	if (window)
+		window.console.log = () => { };
+}
 
 platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+	.catch(err => console.error(err));
